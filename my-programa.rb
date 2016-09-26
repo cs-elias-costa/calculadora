7da937301fbbe18a137f15043a6a4aa437cadc8f
Elias Costa <eliascosta@Supports-MacBook-Pro-2.local>
2016-09-26 16:35:28 -0300

    calculadora

--- my-programa.rb
+++ my-programa.rb
@@ -1,17 +1,27 @@
 
-
-
 puts "Meu primeiro programa \o/"
 
 
 puts "Entre com o valor a ser calculado:"
-num = gets.chomp
+num = Float(gets.chomp)
 
-puts num
+puts "Entre com o operador utilizdo: \n + - * /"
+operador = gets.chomp
 
-
-
-
-
-
-
+if !operador.nil?
+    puts "Entre com o segundo a valor "
+    num2 = Float(gets.chomp)
+    case operador
+    when "+"
+      resultado = num + num2
+    when "-"
+      resultado = num - num2
+    when "*"
+      resultado = num * num2
+    when "/"
+      resultado = num / num2
+    else
+      puts "Operador utilizado é invalido"
+    end
+    puts "Resultado: #{resultado}"
+end
